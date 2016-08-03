ed5639c85042cc33f1732ace9431606d8ab9a2d4
Moritz Kiehn <msmk@cern.ch>
2016-08-03 16:16:56 +0200

    test: extend scripts

---
+++ test/run_dut.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -x
+#
+# run the processing chain for the dut
+
+
+CFG=configs/global.cfg
+REF=configs/device-ref.cfg
+DUT=configs/device-dut0.cfg
+RUN=1066
+FLAGS="--numEvents 10000"
+
+RAWFILE=$(realpath $(printf "raw/cosmic_%06d.root" $RUN))
+RUNDIR=$(printf "run%06d" $RUN)
+PREFIX=$(printf "trees/run%06d-" $RUN)
+
+MACROS=$(realpath "../macros")
+
+source build/activate.sh
+cd $RUNDIR
+
+cp -f comparison/unaligned-dut0.dat alignment-dut0.dat
+
+Judith $FLAGS -c applyMask -i $RAWFILE -o ${PREFIX}dut0.root -t $CFG -r $DUT
+Judith $FLAGS -c noiseScan -i ${PREFIX}dut0.root -t $CFG -r $DUT
+Judith $FLAGS -c applyMask -i $RAWFILE -o ${PREFIX}dut0.root -t $CFG -r $DUT
+# # extra mapping and selection for ccpd devices
+root -l -q "${MACROS}/Regions.cc+(\"${PREFIX}dut0\",0,5,157,173,165,168)"
+root -l -q "${MACROS}/mapping.cc+(\"${PREFIX}dut0_Stime.root\",\"${PREFIX}dut0_Stime-m.root\")"
+# alignment and final analysis
+Judith $FLAGS -c coarseAlignDUT -t $CFG -r $REF -d $DUT \
+    -i ${PREFIX}ref-p.root -I ${PREFIX}dut0_Stime-m.root
+Judith $FLAGS -c fineAlignDUT -t $CFG -r $REF -d $DUT \
+    -i ${PREFIX}ref-p.root -I ${PREFIX}dut0_Stime-m.root
+Judith $FLAGS -c analysisDUT -t $CFG -r $REF -d $DUT \
+    -i ${PREFIX}ref-p.root -I ${PREFIX}dut0_Stime-m.root \
+    -R ${PREFIX}dut0-analysis.root
--- test/run_reference.sh
+++ test/run_reference.sh
@@ -1,25 +1,27 @@
-#!/bin/sh -x
+#!/bin/sh -ex
 #
 # run the processing chain for the reference telescope
 
 CFG=configs/global.cfg
-REF=configs/ref.cfg
-RAWFILE=raw/cosmic_85_0875.root
-RUNDIR=runs/run000875
-PREFIX="${RUNDIR}/run00875-"
+REF=configs/device-ref.cfg
+RUN=1066
+FLAGS="--numEvents 10000 --printLevel 1"
 
-JUDITH="Judith --numEvents 10000 --printLevel 1"
+RAWFILE=$(realpath $(printf "raw/cosmic_%06d.root" $RUN))
+RUNDIR=$(printf "run%06d" $RUN)
+PREFIX=$(printf "trees/run%06d-" $RUN)
 
 source build/activate.sh
-rm -rf $RUNDIR
-mkdir -p $RUNDIR
-cp -f comparison/alignment-ref.dat alignment
+cd $RUNDIR
 
-$JUDITH -c applyMask -i $RAWFILE -o ${PREFIX}ref.root -t $CFG -r $REF
-$JUDITH -c noiseScan -i ${PREFIX}ref.root -t $CFG -r $REF
-$JUDITH -c applyMask -i $RAWFILE -o ${PREFIX}ref.root -t $CFG -r $REF
-$JUDITH -c process -i ${PREFIX}ref.root -o ${PREFIX}ref-p.root -t $CFG -r $REF
-$JUDITH -c coarseAlign -i ${PREFIX}ref-p.root -t $CFG -r $REF
-$JUDITH -c fineAlign -i ${PREFIX}ref-p.root -t $CFG -r $REF
-$JUDITH -c process -i ${PREFIX}ref.root -o ${PREFIX}ref-pa.root -t $CFG -r $REF
-$JUDITH -c analysis -i ${PREFIX}ref-pa.root -R ${PREFIX}ref-pa-analysis.root -t $CFG -r $REF
+cp -f comparison/unaligned-ref.dat alignment-ref.dat
+
+# assume noise-free telescope; no noise mask
+# Judith $FLAGS -c applyMask -i $RAWFILE -o ${PREFIX}ref.root -t $CFG -r $REF
+# Judith $FLAGS -c noiseScan -i ${PREFIX}ref.root -t $CFG -r $REF
+Judith $FLAGS -c applyMask -i $RAWFILE -o ${PREFIX}ref.root -t $CFG -r $REF
+Judith $FLAGS -c process -i ${PREFIX}ref.root -o ${PREFIX}ref-p.root -t $CFG -r $REF
+Judith $FLAGS -c coarseAlign -i ${PREFIX}ref-p.root -t $CFG -r $REF
+Judith $FLAGS -c fineAlign -i ${PREFIX}ref-p.root -t $CFG -r $REF
+Judith $FLAGS -c process -i ${PREFIX}ref.root -o ${PREFIX}ref-pa.root -t $CFG -r $REF
+Judith $FLAGS -c analysis -i ${PREFIX}ref-pa.root -R ${PREFIX}ref-pa-analysis.root -t $CFG -r $REF
