d7cc9ceaedfce098bb7e2585668df40050b0e83a
Moritz Kiehn <msmk@cern.ch>
2016-08-08 17:39:44 +0200

    test: update dut script

--- test/run_dut.sh
+++ test/run_dut.sh
@@ -4,7 +4,8 @@
 
 CFG=configs/global.cfg
 REF=configs/device-ref.cfg
-DUT=configs/device-dut0.cfg
+DUT0=configs/device-dut0.cfg
+DUT0M=configs/device-dut0_mapped.cfg
 RUN=1066
 # FLAGS="--numEvents 10000"
 FLAGS=
@@ -18,20 +19,31 @@ MACROS=$(realpath "../macros")
 source build/activate.sh
 cd $RUNDIR
 
-cp -f comparison/unaligned-dut0.dat alignment-dut0.dat
+# use prealigned geometry since alignment does currently not work
+cp -f comparison/alignment-ref.dat alignment-ref.dat
+cp -f comparison/alignment-dut0.dat alignment-dut0.dat
 mkdir -p output
 
-Judith $FLAGS -c applyMask -i $RAWFILE -o ${PREFIX}dut0.root -t $CFG -r $DUT
-Judith $FLAGS -c noiseScan -i ${PREFIX}dut0.root -t $CFG -r $DUT
-Judith $FLAGS -c applyMask -i $RAWFILE -o ${PREFIX}dut0.root -t $CFG -r $DUT
-# # extra mapping and selection for ccpd devices
+Judith $FLAGS -c applyMask -i $RAWFILE -o ${PREFIX}dut0.root -t $CFG -r $DUT0
+Judith $FLAGS -c noiseScan -i ${PREFIX}dut0.root -t $CFG -r $DUT0
+Judith $FLAGS -c applyMask -i $RAWFILE -o ${PREFIX}dut0.root -t $CFG -r $DUT0
+# extra mapping and selection for ccpd devices
 root -l -q "${MACROS}/Regions.cc+(\"${PREFIX}dut0\",0,5,157,173,165,168)"
 root -l -q "${MACROS}/mapping.cc+(\"${PREFIX}dut0_Stime.root\",\"${PREFIX}dut0_Stime-m.root\")"
 # alignment and final analysis
-Judith $FLAGS -c coarseAlignDUT -t $CFG -r $REF -d $DUT \
-    -i ${PREFIX}ref-p.root -I ${PREFIX}dut0_Stime-m.root
-Judith $FLAGS -c fineAlignDUT -t $CFG -r $REF -d $DUT \
-    -i ${PREFIX}ref-p.root -I ${PREFIX}dut0_Stime-m.root
-Judith $FLAGS -c analysisDUT -t $CFG -r $REF -d $DUT \
-    -i ${PREFIX}ref-p.root -I ${PREFIX}dut0_Stime-m.root \
-    -R ${PREFIX}dut0-analysis.root
+
+# TODO 2016-08-08 msmk: dut alignment and analysis does not work due to
+# hard-coded region-of-interest cuts in the code base.
+
+# Judith $FLAGS -c coarseAlignDUT -t $CFG -r $REF -d $DUT \
+#     -i ${PREFIX}ref-p.root -I ${PREFIX}dut0_Stime-m.root
+# Judith $FLAGS -c fineAlignDUT -t $CFG -r $REF -d $DUT \
+#     -i ${PREFIX}ref-p.root -I ${PREFIX}dut0_Stime-m.root
+Judith $FLAGS -c process -t $CFG -r $DUT0M \
+    -i ${PREFIX}dut0_Stime-m.root \
+    -o ${PREFIX}dut0_Stime-p.root \
+    -R ${PREFIX}dut0_Stime-p-hists.root
+# Judith $FLAGS -c analysisDUT -t $CFG -r $REF -d $DUT0M \
+#     -i ${PREFIX}ref-p.root \
+#     -I ${PREFIX}dut0_Stime-p.root \
+#     -R ${PREFIX}dut0_Stime-analysis.root
