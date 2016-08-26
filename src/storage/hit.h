1c0887a97d89a683d4b4e6891590cbfe9a968a98
Moritz Kiehn <msmk@cern.ch>
2016-08-26 16:47:48 +0200

    storage/hit: clean up

--- src/storage/hit.h
+++ src/storage/hit.h
@@ -9,36 +9,28 @@ class Cluster;
 class Plane;
 
 class Hit {
-protected:
-  Hit(); // Hits memory is managed by the event class
-
 public:
-  friend class Plane;     // Access set plane method
-  friend class Event;     // Access cluster index
-  friend class StorageIO; // Constructor and destructor
-
-  // Inline setters and getters since they will be used frequently
-  inline void setPix(unsigned int x, unsigned int y)
+  void setPix(unsigned int x, unsigned int y)
   {
     _pixX = x;
     _pixY = y;
   }
-  inline void setPos(double x, double y, double z)
+  void setPos(double x, double y, double z)
   {
     _posX = x;
     _posY = y;
     _posZ = z;
   }
-  inline void setValue(double value) { _value = value; }
-  inline void setTiming(double timing) { _timing = timing; }
+  void setValue(double value) { _value = value; }
+  void setTiming(double timing) { _timing = timing; }
 
-  inline unsigned int getPixX() const { return _pixX; }
-  inline unsigned int getPixY() const { return _pixY; }
-  inline double getPosX() const { return _posX; }
-  inline double getPosY() const { return _posY; }
-  inline double getPosZ() const { return _posZ; }
-  inline double getValue() const { return _value; }
-  inline double getTiming() const { return _timing; }
+  unsigned int getPixX() const { return _pixX; }
+  unsigned int getPixY() const { return _pixY; }
+  double getPosX() const { return _posX; }
+  double getPosY() const { return _posY; }
+  double getPosZ() const { return _posZ; }
+  double getValue() const { return _value; }
+  double getTiming() const { return _timing; }
 
   void setCluster(Cluster* cluster);
 
@@ -49,10 +41,9 @@ public:
   void print() const;
   const std::string printStr(int blankWidth = 0) const;
 
-protected:
-  Plane* _plane; // Plane in which the hit is found
-
 private:
+  Hit(); // Hits memory is managed by the event class
+
   unsigned int _pixX; //<! pixel number (local x) of this hit
   unsigned int _pixY; //<! pixel number (local y) of this hit
   double _posX;       //<! X (global) position of the hit on the sensor
@@ -62,9 +53,13 @@ private:
   double _timing;     // Level 1 accept, typically
 
   Cluster* _cluster; // The cluster containing this hit
+  Plane* _plane; // Plane in which the hit is found
 
-}; // end of class
+  friend class Plane;     // Access set plane method
+  friend class Event;     // Access cluster index
+  friend class StorageIO; // Constructor and destructor
+};
 
-} // end of namespace
+} // namespace Storage
 
 #endif // HIT_H
