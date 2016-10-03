0a0ed795fea76f2004071fe233a26d43a704b33d
Moritz Kiehn <msmk@cern.ch>
2016-10-03 13:44:18 +0200

    api fixes

--- src/processors/applyalignment.cpp
+++ src/processors/applyalignment.cpp
@@ -29,47 +29,22 @@ void Processors::applyAlignment(Storage::Event* event,
 {
   assert(event && device &&
          "Processors: can't apply alignmet with null event and/or device");
-  assert(event->getNumPlanes() == device->getNumSensors() &&
+  assert(event->numPlanes() == device->getNumSensors() &&
          "Processors: plane / sensor mismatch");
 
-  for (unsigned int nplane = 0; nplane < event->getNumPlanes(); nplane++) {
+  for (unsigned int nplane = 0; nplane < event->numPlanes(); nplane++) {
     Storage::Plane* plane = event->getPlane(nplane);
     const Mechanics::Sensor* sensor = device->getSensor(nplane);
-    // Apply alignment to hits
-    for (unsigned int nhit = 0; nhit < plane->getNumHits(); nhit++) {
-      Storage::Hit* hit = plane->getHit(nhit);
-      double posX = 0, posY = 0, posZ = 0;
-      // std::cout << "At plane: " << nplane << std::endl;
-      sensor->pixelToSpace(
-          hit->getPixX() + 0.5, hit->getPixY() + 0.5, posX, posY, posZ);
-      hit->setPos(posX, posY, posZ);
-      //  cout<<"Sensor= "<<nplane  <<"in processors,hit , set pos
-      //  Z="<<posZ<<std::endl;
-    }
+    const Transform3D pixelToGlobal = sensor->constructPixelToGlobal();
 
-    // Apply alignment to clusters
-    for (unsigned int ncluster = 0; ncluster < plane->getNumClusters();
-         ncluster++) {
-      Storage::Cluster* cluster = plane->getCluster(ncluster);
-      double posX = 0, posY = 0, posZ = 0;
-      // std::cout << "At plane: " << nplane << std::endl;
-      sensor->pixelToSpace(
-          cluster->getPixX(), cluster->getPixY(), posX, posY, posZ);
-      cluster->setPos(posX, posY, posZ);
-      // cout<<"Sensor= "<<nplane  <<"in processors,cluster , set pos
-      // Z="<<posZ<<std::endl;
-
-      XYZVector errLocal(sensor->getPitchX() * cluster->getPixErrX(),
-                         sensor->getPitchY() * cluster->getPixErrY(),
-                         0);
-      XYZVector errGlobal = sensor->localToGlobal() * errLocal;
-      cluster->setPosErr(
-          fabs(errGlobal.x()), fabs(errGlobal.y()), fabs(errGlobal.z()));
-    }
+    for (unsigned int ihit = 0; ihit < plane->numHits(); ihit++)
+      plane->getHit(ihit)->transformToGlobal(pixelToGlobal);
+    for (unsigned int iclu = 0; iclu < plane->numClusters(); iclu++)
+      plane->getCluster(iclu)->transformToGlobal(pixelToGlobal);
   }
 
   // Apply alignment to tracks
-  for (unsigned int ntrack = 0; ntrack < event->getNumTracks(); ntrack++) {
+  for (unsigned int ntrack = 0; ntrack < event->numTracks(); ntrack++) {
     Storage::Track* track = event->getTrack(ntrack);
     Processors::TrackMaker::fitTrackToClusters(track);
   }
