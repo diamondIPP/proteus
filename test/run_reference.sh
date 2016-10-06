b4ea1629bd2913ac956538336fe0a4e98a3e714d
Moritz Kiehn <msmk@cern.ch>
2016-10-06 12:02:15 +0200

    test: update scripts to use toml configs

--- test/run_reference.sh
+++ test/run_reference.sh
@@ -4,7 +4,7 @@
 
 RUN=${1-1066}
 CFG=configs/global.cfg
-REF=configs/device-ref.cfg
+REF=configs/device-ref.toml
 # FLAGS="--numEvents 10000 --printLevel 1"
 # FLAGS="--numEvents 100000"
 FLAGS=
@@ -16,14 +16,14 @@ PREFIX=$(printf "output/run%06d-" $RUN)
 source build/activate.sh
 cd $RUNDIR
 
-cp -f comparison/unaligned-ref.dat alignment-ref.dat
+cp -f comparison/unaligned.toml alignment-ref.toml
 mkdir -p output
 
 # assume noise-free telescope; no noise mask
 proteus $FLAGS -c applyMask -i $RAWFILE -o ${PREFIX}ref.root -t $CFG -r $REF
 proteus $FLAGS -c process -i ${PREFIX}ref.root -o ${PREFIX}ref-unaligned.root -t $CFG -r $REF -R ${PREFIX}ref-unaligned-hists.root
 proteus $FLAGS -c coarseAlign -i ${PREFIX}ref-unaligned.root -t $CFG -r $REF
-cp alignment-ref.dat alignment-ref-coarseAlign.dat
+cp alignment-ref.toml alignment-ref-coarse.toml
 # TODO 2016-09-16 msmk: fine alignment does not work w/ roi cut on data
 # proteus $FLAGS -c fineAlign -i ${PREFIX}ref-unaligned.root -t $CFG -r $REF
 # cp alignment-ref.dat alignment-ref-fineAlign.dat
