ef7b220cc52d5328e5debfdb54a1081a8dbfec8b
Moritz Kiehn <msmk@cern.ch>
2016-10-20 14:26:09 +0200

    utils/arguments: support arbitrary optional arguments

--- src/utils/arguments.cpp
+++ src/utils/arguments.cpp
@@ -1,18 +1,41 @@
 #include "arguments.h"
 
+#include <cstdint>
 #include <cstring>
 #include <iostream>
+#include <limits>
 #include <vector>
+
+#include "utils/logger.h"
+
+using Utils::logger;
+
+Utils::Arguments::Optional::Optional(char abbreviation_,
+                                     const std::string& name_,
+                                     const std::string& help_,
+                                     const std::string& default_)
+    : name(name_)
+    , help(help_)
+    , value(default_)
+    , abbreviation(abbreviation_)
+{
+}
 
 Utils::Arguments::Arguments(const char* description)
     : m_description(description)
 {
-  // default values
-  m_opts["-d"] = "configs/device.toml";
-  m_opts["-c"] = "configs/analysis.toml";
+  // default optional arguments
+  m_optional.emplace_back(
+      'd', "device", "device description", "configs/device.toml");
+  m_optional.emplace_back(
+      'c', "config", "configuration", "configs/analysis.toml");
+  m_optional.emplace_back(
+      's', "skip_events", "skip the first n events", std::to_string(0));
+  m_optional.emplace_back(
+      'n', "num_events", "number of events to process", std::to_string(-1));
 }
 
-void Utils::Arguments::printHelp(const std::string& arg0)
+void Utils::Arguments::printHelp(const std::string& arg0) const
 {
   using std::cerr;
 
@@ -23,9 +46,38 @@ void Utils::Arguments::printHelp(const std::string& arg0)
   cerr << m_description << '\n';
   cerr << '\n';
   cerr << "options:\n";
-  cerr << "  -d PATH  device description (default=" << device() << ") \n";
-  cerr << "  -c PATH  configuration (default=" << config() << ")\n";
+  for (auto opt = m_optional.begin(); opt != m_optional.end(); ++opt) {
+    cerr << "  ";
+    if (opt->abbreviation != 0)
+      cerr << "-" << opt->abbreviation;
+    cerr << ",--" << opt->name;
+    cerr << " \t" << opt->help;
+    if (!opt->value.empty()) {
+      cerr << " (default=" << opt->value << ')';
+    }
+    cerr << '\n';
+  }
   cerr.flush();
+}
+
+Utils::Arguments::Optional* Utils::Arguments::find(const std::string& name,
+                                                   char abbreviation)
+{
+  for (auto opt = m_optional.begin(); opt != m_optional.end(); ++opt) {
+    if ((opt->name == name) || (opt->abbreviation == abbreviation))
+      return &(*opt);
+  }
+  return NULL;
+}
+
+const Utils::Arguments::Optional*
+Utils::Arguments::find(const std::string& name, char abbreviation) const
+{
+  for (auto opt = m_optional.begin(); opt != m_optional.end(); ++opt) {
+    if ((opt->name == name) || (opt->abbreviation == abbreviation))
+      return &(*opt);
+  }
+  return NULL;
 }
 
 bool Utils::Arguments::parse(int argc, char const* argv[])
@@ -36,35 +88,55 @@ bool Utils::Arguments::parse(int argc, char const* argv[])
   }
 
   // separated optional flags and positional arguments
-  std::vector<std::string> positional;
+  m_positional.clear();
   for (int i = 1; i < argc; ++i) {
-    auto opt = m_opts.find(argv[i]);
-    if (opt != m_opts.end()) {
-      // known optional argument
-      if ((i + 1) < argc) {
-        opt->second = argv[++i];
+    std::string arg(argv[i]);
+
+    if ((arg.find("--") == 0) && (2 < arg.size())) {
+      // long optional argument
+      DEBUG("long option: ", arg, ' ', argv[i + 1], '\n');
+      Optional* opt = find(arg.substr(2), 0);
+      if (!opt) {
+        std::cerr << "unknown long option '" << arg << "'\n\n";
+        return true;
       }
-    } else if (argv[i][0] == '-') {
-      // unkown optional argument
-      std::cerr << "unknown argument: " << argv[i] << "\n\n";
-      return true;
+      if ((i + 1) < argc)
+        opt->value = argv[++i];
+    } else if ((arg.find("-") == 0) && (1 < arg.size())) {
+      DEBUG("short option: ", arg, ' ', argv[i + 1], '\n');
+      // short optional argument
+      Optional* opt = find("", arg[1]);
+      if (!opt) {
+        std::cerr << "unknown short option '" << arg << "'\n\n";
+        return true;
+      }
+      if ((i + 1) < argc)
+        opt->value = argv[++i];
     } else {
+      DEBUG("positional argument: ", arg, '\n');
       // positional argument
-      positional.emplace_back(argv[i]);
+      m_positional.emplace_back(std::move(arg));
     }
   }
 
-  if (positional.size() != 2) {
+  if (m_positional.size() != 2) {
     std::cerr << "incorrect number of arguments\n\n";
     return true;
   }
-  m_input = positional[0];
-  m_outputPrefix = positional[1];
+  m_input = m_positional[0];
+  m_outputPrefix = m_positional[1];
   return false;
 }
 
-const std::string& Utils::Arguments::device() const { return m_opts.at("-d"); }
-const std::string& Utils::Arguments::config() const { return m_opts.at("-c"); }
+std::string Utils::Arguments::device() const
+{
+  return get<std::string>("device");
+}
+
+std::string Utils::Arguments::config() const
+{
+  return get<std::string>("config");
+}
 
 std::string Utils::Arguments::makeOutput(const std::string& name) const
 {
--- src/utils/arguments.h
+++ src/utils/arguments.h
@@ -7,9 +7,11 @@
 #ifndef PT_ARGUMENTS_H
 #define PT_ARGUMENTS_H
 
-#include <map>
 #include <ostream>
+#include <sstream>
+#include <stdexcept>
 #include <string>
+#include <vector>
 
 namespace Utils {
 
@@ -23,21 +25,50 @@ public:
 
   const std::string& input() const { return m_input; }
   const std::string& outputPrefix() const { return m_outputPrefix; }
-  const std::string& device() const;
-  const std::string& config() const;
+  std::string device() const;
+  std::string config() const;
   /** Construct full output path from known output prefix and name. */
   std::string makeOutput(const std::string& name) const;
+  /** Access an optional argument and convert it to the requested type. */
+  template <typename T> T get(const std::string& name) const;
 
   void print(std::ostream& os) const;
 
 private:
-  void printHelp(const std::string& arg0);
+  struct Optional {
+    const std::string name;
+    const std::string help;
+    std::string value;
+    const char abbreviation;
+
+    Optional(char abbreviation_,
+             const std::string& name_,
+             const std::string& help_,
+             const std::string& default_ = std::string());
+  };
+
+  void printHelp(const std::string& arg0) const;
+  Optional* find(const std::string& name, char abbreviation);
+  const Optional* find(const std::string& name, char abbreviation) const;
 
   const std::string m_description;
+  std::vector<std::string> m_positional;
+  std::vector<Optional> m_optional;
   std::string m_input, m_outputPrefix;
-  std::map<std::string, std::string> m_opts;
 };
 
 } // namespace Utils
+
+template <typename T> T Utils::Arguments::get(const std::string& name) const
+{
+  const Optional* opt = find(name, 0);
+  if (!opt)
+    throw std::runtime_error("Arguments: option '" + name +
+                             "' does not exists");
+
+  T ret;
+  std::istringstream(opt->value) >> ret;
+  return ret;
+}
 
 #endif // PT_ARGUMENTS_H
