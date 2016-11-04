4ea9d7a9483629fcda545e12554abbf57336ec9d
Moritz Kiehn <msmk@cern.ch>
2016-11-04 12:55:16 +0100

    hitmapper: use digital/physical address

--- src/processors/hitmapper.cpp
+++ src/processors/hitmapper.cpp
@@ -22,15 +22,15 @@ void Processors::CCPDv4HitMapper::process(Storage::Event& event) const
   for (auto id = m_sensorIds.begin(); id != m_sensorIds.end(); ++id) {
     Storage::Plane* plane = event.getPlane(*id);
     for (Index ihit = 0; ihit < plane->numHits(); ++ihit) {
-      Storage::Hit* hit = plane->getHit(ihit);
+      Storage::Hit& hit = *plane->getHit(ihit);
 
       // two hits in digital column correspond to two hits in a sensor row.
       // * lower digital hit -> left sensor hit
       // * upper digital hit -> right sensor hit
       // translated from the mapping.cc ROOT script
       // TODO 2016-10-13 msmk: assumes correct mapping, i.e. even to even
-      Index fei4Col = hit->col();
-      Index fei4Row = hit->row();
+      Index fei4Col = hit.digitalCol();
+      Index fei4Row = hit.digitalRow();
       Index col, row;
       if ((fei4Col % 2) == 0) {
         if ((fei4Row % 2) != 0)
@@ -44,7 +44,7 @@ void Processors::CCPDv4HitMapper::process(Storage::Event& event) const
           col = 2 * fei4Col;
       }
       row = fei4Row / 2;
-      hit->setAddress(col, row);
+      hit.setPhysicalAddress(col, row);
     }
   }
 }
