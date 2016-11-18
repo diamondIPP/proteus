2f3539244db38605d6d0a54f5eaa168659704254
Moritz Kiehn <msmk@cern.ch>
2016-11-18 14:44:39 +0100

    analyzers/correlation: generalize for arbitrary sensors

--- src/analyzers/correlation.cpp
+++ src/analyzers/correlation.cpp
@@ -8,100 +8,18 @@
 #include "mechanics/device.h"
 #include "storage/event.h"
 
-void Analyzers::Correlation::processEvent(const Storage::Event* event)
+Analyzers::Correlation::Correlation(const Mechanics::Device& device,
+                                    const std::vector<Index>& sensorIds,
+                                    TDirectory* dir)
+    : SingleAnalyzer(&device, dir, "", "Correlation")
 {
-  assert(event && "Analyzer: can't process null events");
+  if (sensorIds.size() < 2)
+    throw std::runtime_error("Correlation: need at least two sensors");
 
-  // Throw an error for sensor / plane mismatch
-  eventDeviceAgree(event);
-
-  if (!checkCuts(event))
-    return;
-
-  for (auto hist = m_hists.begin(); hist != m_hists.end(); ++hist) {
-    const Storage::Plane& plane0 = *event->getPlane(hist->id0);
-    const Storage::Plane& plane1 = *event->getPlane(hist->id1);
-
-    for (Index ic0 = 0; ic0 < plane0.numClusters(); ++ic0) {
-      const Storage::Cluster* cluster0 = plane0.getCluster(ic0);
-      const XYZPoint& xyz0 = cluster0->posGlobal();
-      if (!checkCuts(cluster0))
-        continue;
-
-      for (Index ic1 = 0; ic1 < plane1.numClusters(); ++ic1) {
-        const Storage::Cluster* cluster1 = plane1.getCluster(ic1);
-        const XYZPoint& xyz1 = cluster1->posGlobal();
-        if (!checkCuts(cluster1))
-          continue;
-
-        hist->corrX->Fill(xyz0.x(), xyz1.x());
-        hist->corrY->Fill(xyz0.y(), xyz1.y());
-        hist->diffX->Fill(xyz1.x() - xyz0.x());
-        hist->diffY->Fill(xyz1.y() - xyz0.y());
-      }
-    }
+  TDirectory* corrDir = makeGetDirectory("Correlations");
+  for (auto id = sensorIds.begin(); (id + 1) != sensorIds.end(); ++id) {
+    addHist(*device.getSensor(*id), *device.getSensor(*(id + 1)), corrDir);
   }
-}
-
-void Analyzers::Correlation::postProcessing() {}
-
-void Analyzers::Correlation::addHist(const Mechanics::Sensor& sensor0,
-                                     const Mechanics::Sensor& sensor1,
-                                     TDirectory* dir)
-{
-  int nbins = 100;
-  auto makeCorr = [&](int axis) -> TH2D* {
-    std::string aname = (axis = 0) ? "X" : "Y";
-    std::string name = sensor0.name() + "-" + sensor1.name() + "-Corr" + aname;
-    std::string xlabel = sensor0.name() + " cluster " + aname;
-    std::string ylabel = sensor1.name() + " cluster " + aname;
-    auto range0 = sensor0.sensitiveEnvelopeGlobal().axes[axis];
-    auto range1 = sensor1.sensitiveEnvelopeGlobal().axes[axis];
-    TH2D* h = new TH2D(name.c_str(), "", nbins, range0.min, range0.max, nbins,
-                       range1.min, range1.max);
-    h->GetXaxis()->SetTitle(xlabel.c_str());
-    h->GetYaxis()->SetTitle(ylabel.c_str());
-    h->SetDirectory(dir);
-    return h;
-  };
-  auto makeDiff = [&](int axis) -> TH1D* {
-    std::string aname = (axis = 0) ? "X" : "Y";
-    std::string name = sensor0.name() + "-" + sensor1.name() + "-Diff" + aname;
-    std::string xlabel =
-        sensor1.name() + " - " + sensor0.name() + " cluster " + aname;
-    auto range0 = sensor0.sensitiveEnvelopeGlobal().axes[axis];
-    auto range1 = sensor0.sensitiveEnvelopeGlobal().axes[axis];
-    auto length = (range0.length() + range1.length()) / 2;
-    TH1D* h = new TH1D(name.c_str(), "", nbins, -length, length);
-    h->GetXaxis()->SetTitle(xlabel.c_str());
-    h->SetDirectory(dir);
-    return h;
-  };
-
-  CorrelationHists hist;
-  hist.id0 = sensor0.id();
-  hist.id1 = sensor1.id();
-  hist.corrX = makeCorr(0);
-  hist.corrY = makeCorr(1);
-  hist.diffX = makeDiff(0);
-  hist.diffY = makeDiff(1);
-  m_hists.push_back(hist);
-}
-
-TH1D* Analyzers::Correlation::getAlignmentPlotX(Index sensorId)
-{
-  if (sensorId == 0)
-    throw std::runtime_error("Correlation: no plot exists for sensor 0");
-  // TODO 2016-11-16 msmk: check if correct histogram
-  return m_hists.at(sensorId - 1).diffX;
-}
-
-TH1D* Analyzers::Correlation::getAlignmentPlotY(Index sensorId)
-{
-  if (sensorId == 0)
-    throw std::runtime_error("Correlation: no plot exists for sensor 0");
-  // TODO 2016-11-16 msmk: check if correct histogram
-  return m_hists.at(sensorId - 1).diffY;
 }
 
 Analyzers::Correlation::Correlation(const Mechanics::Device* device,
@@ -114,6 +32,117 @@ Analyzers::Correlation::Correlation(const Mechanics::Device* device,
 
   TDirectory* corrDir = makeGetDirectory("Correlations");
   for (Index sensorId = 0; (sensorId + 1) < device->numSensors(); ++sensorId)
-    addHist(*_device->getSensor(sensorId), *_device->getSensor(sensorId + 1),
+    addHist(*device->getSensor(sensorId), *device->getSensor(sensorId + 1),
             corrDir);
+}
+
+void Analyzers::Correlation::addHist(const Mechanics::Sensor& sensor0,
+                                     const Mechanics::Sensor& sensor1,
+                                     TDirectory* dir)
+{
+  auto makeCorr = [&](int axis) -> TH2D* {
+    std::string aname = (axis == 0) ? "X" : "Y";
+    std::string name = sensor0.name() + "-" + sensor1.name() + "-Corr" + aname;
+    std::string xlabel = sensor0.name() + " cluster " + aname;
+    std::string ylabel = sensor1.name() + " cluster " + aname;
+    auto range0 = sensor0.projectedEnvelopeXY().axes[axis];
+    auto range1 = sensor1.projectedEnvelopeXY().axes[axis];
+    auto pitch0 = sensor0.projectedPitchXY()[axis];
+    auto pitch1 = sensor1.projectedPitchXY()[axis];
+    TH2D* h =
+        new TH2D(name.c_str(), "", range0.length() / pitch0, range0.min,
+                 range0.max, range1.length() / pitch1, range1.min, range1.max);
+    h->GetXaxis()->SetTitle(xlabel.c_str());
+    h->GetYaxis()->SetTitle(ylabel.c_str());
+    h->SetDirectory(dir);
+    return h;
+  };
+  auto makeDiff = [&](int axis) -> TH1D* {
+    std::string aname = (axis == 0) ? "X" : "Y";
+    std::string name = sensor0.name() + "-" + sensor1.name() + "-Diff" + aname;
+    std::string xlabel =
+        sensor1.name() + " - " + sensor0.name() + " cluster " + aname;
+    auto range0 = sensor0.projectedEnvelopeXY().axes[axis];
+    auto range1 = sensor0.projectedEnvelopeXY().axes[axis];
+    auto pitch0 = sensor0.projectedPitchXY()[axis];
+    auto pitch1 = sensor1.projectedPitchXY()[axis];
+    auto pitch = std::min(pitch0, pitch1);
+    auto length = range0.length() + range1.length();
+    TH1D* h =
+        new TH1D(name.c_str(), "", length / pitch, -length / 2, length / 2);
+    h->GetXaxis()->SetTitle(xlabel.c_str());
+    h->SetDirectory(dir);
+    return h;
+  };
+
+  Hists hist;
+  hist.corrX = makeCorr(0);
+  hist.corrY = makeCorr(1);
+  hist.diffX = makeDiff(0);
+  hist.diffY = makeDiff(1);
+  m_hists[std::make_pair(sensor0.id(), sensor1.id())] = hist;
+}
+
+void Analyzers::Correlation::processEvent(const Storage::Event* event)
+{
+  assert(event && "Analyzer: can't process null events");
+
+  // Throw an error for sensor / plane mismatch
+  eventDeviceAgree(event);
+
+  if (!checkCuts(event))
+    return;
+
+  for (auto it = m_hists.begin(); it != m_hists.end(); ++it) {
+    Index id0 = it->first.first;
+    Index id1 = it->first.second;
+    const Hists& hist = it->second;
+    const Storage::Plane& plane0 = *event->getPlane(id0);
+    const Storage::Plane& plane1 = *event->getPlane(id1);
+
+    for (Index c0 = 0; c0 < plane0.numClusters(); ++c0) {
+      const Storage::Cluster* cluster0 = plane0.getCluster(c0);
+      const XYZPoint& xyz0 = cluster0->posGlobal();
+      if (!checkCuts(cluster0))
+        continue;
+
+      for (Index c1 = 0; c1 < plane1.numClusters(); ++c1) {
+        const Storage::Cluster* cluster1 = plane1.getCluster(c1);
+        const XYZPoint& xyz1 = cluster1->posGlobal();
+        if (!checkCuts(cluster1))
+          continue;
+
+        hist.corrX->Fill(xyz0.x(), xyz1.x());
+        hist.corrY->Fill(xyz0.y(), xyz1.y());
+        hist.diffX->Fill(xyz1.x() - xyz0.x());
+        hist.diffY->Fill(xyz1.y() - xyz0.y());
+      }
+    }
+  }
+}
+
+void Analyzers::Correlation::postProcessing() {}
+
+TH1D* Analyzers::Correlation::getHistDiffX(Index sensorId0,
+                                           Index sensorId1) const
+{
+  auto idx = std::make_pair(sensorId0, sensorId1);
+  return m_hists.at(idx).diffX;
+}
+
+TH1D* Analyzers::Correlation::getHistDiffY(Index sensorId0,
+                                           Index sensorId1) const
+{
+  auto idx = std::make_pair(sensorId0, sensorId1);
+  return m_hists.at(idx).diffY;
+}
+
+TH1D* Analyzers::Correlation::getAlignmentPlotX(Index sensorId) const
+{
+  return getHistDiffX(sensorId - 1, sensorId);
+}
+
+TH1D* Analyzers::Correlation::getAlignmentPlotY(Index sensorId) const
+{
+  return getHistDiffY(sensorId - 1, sensorId);
 }
--- src/analyzers/correlation.h
+++ src/analyzers/correlation.h
@@ -1,7 +1,7 @@
 #ifndef PT_CORRELATION_H
 #define PT_CORRELATION_H
 
-#include <vector>
+#include <map>
 
 #include <TDirectory.h>
 #include <TH1D.h>
@@ -22,6 +22,10 @@ namespace Analyzers {
 
 class Correlation : public SingleAnalyzer {
 public:
+  /** Consider pair-wise correlations between neighboring sensors. */
+  Correlation(const Mechanics::Device& device,
+              const std::vector<Index>& sensorIds,
+              TDirectory* dir);
   Correlation(const Mechanics::Device* device,
               TDirectory* dir,
               const char* suffix = "");
@@ -29,8 +33,12 @@ public:
   void processEvent(const Storage::Event* event);
   void postProcessing();
 
-  TH1D* getAlignmentPlotX(Index sensorId);
-  TH1D* getAlignmentPlotY(Index sensorId);
+  TH1D* getHistDiffX(Index sensorId0, Index sensorId1) const;
+  TH1D* getHistDiffY(Index sensorId0, Index sensorId1) const;
+  /** \deprecated Use getHistDiffX instead. */
+  TH1D* getAlignmentPlotX(Index sensorId) const;
+  /** \deprecated Use getHistDiffY instead. */
+  TH1D* getAlignmentPlotY(Index sensorId) const;
 
 private:
   // Shared function to initialize the correlation hist between two sensors
@@ -38,14 +46,15 @@ private:
                const Mechanics::Sensor& sensor1,
                TDirectory* dir);
 
-  struct CorrelationHists {
-    Index id0, id1;
+  typedef std::pair<Index, Index> Indices;
+  struct Hists {
     TH2D* corrX;
     TH2D* corrY;
     TH1D* diffX;
     TH1D* diffY;
   };
-  std::vector<CorrelationHists> m_hists;
+
+  std::map<Indices, Hists> m_hists;
 };
 
 } // namespace Analyzers
