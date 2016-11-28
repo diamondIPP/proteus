067c361d3bfc9c30d6d4f707160cd431efdb2d01
Moritz Kiehn <msmk@cern.ch>
2016-11-28 18:36:17 +0100

    test: fix dut0 noisescan script

--- test/run_dut0_noisescan.sh
+++ test/run_dut0_noisescan.sh
@@ -10,5 +10,5 @@ rawfile=$(printf "raw/run%06d.root" $RUN)
 prefix=$(printf "output/run%06d-" $RUN)
 
 pt-noisescan $FLAGS -d ${rundir}/device.toml \
-  -c ${rundir}/configs/noisescan_dut0.toml \
+  -c ${rundir}/configs/dut0_noisescan.toml \
   ${rawfile} ${prefix}dut0_noisescan
