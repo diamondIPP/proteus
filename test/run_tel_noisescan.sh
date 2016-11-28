e5c9bad3d7ef673a2c940c6bb4085efb933c857c
Moritz Kiehn <msmk@cern.ch>
2016-11-28 15:35:28 +0100

    test: split example scripts

---
+++ test/run_tel_noisescan.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -ex
+#
+# run the noisescan for the telescope
+
+RUN=$1; shift
+FLAGS=$@ # e.g. -n 10000, to process only the first 10k events
+
+rundir=$(printf "run%06d" $RUN)
+rawfile=$(printf "raw/run%06d.root" $RUN)
+prefix=$(printf "output/run%06d-" $RUN)
+
+source build/activate.sh
+mkdir -p output
+
+pt-noisescan $FLAGS -d ${rundir}/device.toml \
+  -c ${rundir}/configs/tel_noisescan.toml \
+  ${rawfile} ${prefix}tel_noisescan
---
+++ test/run_track.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -ex
+#
+# run telescope tracking
+
+RUN=$1; shift
+FLAGS=$@ # e.g. -n 10000, to process only the first 10k events
+
+rundir=$(printf "run%06d" $RUN)
+rawfile=$(printf "raw/run%06d.root" $RUN)
+prefix=$(printf "output/run%06d-" $RUN)
+
+source build/activate.sh
+mkdir -p output
+
+pt-track $FLAGS -d ${rundir}/device.toml \
+  -c ${rundir}/configs/analysis.toml \
+  ${rawfile} ${prefix}track
