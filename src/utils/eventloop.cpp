4d2bdaff7259e1070ec015e7cddd80f447d23296
Moritz Kiehn <msmk@cern.ch>
2016-11-30 15:20:53 +0100

    utils/progressbar: fix initialization and display

--- src/utils/eventloop.cpp
+++ src/utils/eventloop.cpp
@@ -111,9 +111,7 @@ void Utils::EventLoop::run()
 
   Utils::ProgressBar progress;
   Time startWall = Clock::now();
-  progress.update(0);
   for (uint64_t ievent = m_startEvent; ievent <= m_endEvent; ievent++) {
-
     {
       Time start = Clock::now();
       m_input->readEvent(ievent, &event);
--- src/utils/progressbar.h
+++ src/utils/progressbar.h
@@ -15,46 +15,53 @@ namespace Utils {
 /** Display an updatable progress bar on a single output line */
 class ProgressBar {
 public:
-  /**
+  /** Initialize progress bar and display empty progress.
+   *
    * \param lineLength  Total length of the output line
    */
-  ProgressBar(size_t lineLength = 79)
-      : m_length((lineLength < 8) ? 0 : (lineLength - 8))
+  ProgressBar(int lineLength = 79)
+      : m_os(std::cout)
       , m_curr(0)
-      , m_os(std::cout)
+      , m_max((lineLength < 9) ? 0 : (lineLength - 9))
   {
+    display();
   }
   /** Update the progress bar if necessary. Fraction must be in [0,1]. */
   void update(float fraction)
   {
-    size_t status = fraction * m_length;
+    int status = fraction * m_max;
     // only update if neccessary
-    if (status == m_curr)
-      return;
-    m_os << "[";
-    for (size_t i = 0; i < status; ++i)
-      m_os << '=';
-    for (size_t i = status; i < m_length; ++i)
-      m_os << ' ';
-    // after printing rewind back to the beginning of the line so that the next
-    // update (or unrelated messages) overwrite the current status
-    m_os << "] " << std::setw(3) << static_cast<size_t>(100 * fraction) << "%\r"
-         << std::flush;
-    // update for next iteration
-    m_curr = status;
+    if (m_curr < status) {
+      m_curr = status;
+      display();
+    }
   }
   /** Overwrite the progress bar with empty spaces. */
   void clear()
   {
-    for (size_t i = 0; i < (m_length + 8); ++i)
+    for (int i = 0; i <= (m_max + 8); ++i)
       m_os << ' ';
     m_os << '\r' << std::flush;
   }
 
 private:
-  const size_t m_length;
-  size_t m_curr;
+  /** Display the current progress status. */
+  void display()
+  {
+    m_os << "[";
+    for (int i = 0; i < m_curr; ++i)
+      m_os << '=';
+    for (int i = m_curr; i <= m_max; ++i)
+      m_os << ' ';
+    // after printing rewind back to the beginning of the line so that the
+    // next update (or unrelated messages) overwrite the current status
+    int percent = static_cast<int>(100.0f * m_curr / m_max);
+    m_os << "] " << std::setw(3) << percent << "%\r" << std::flush;
+  }
+
   std::ostream& m_os;
+  int m_curr;
+  int m_max;
 };
 
 } // namespace Utils
