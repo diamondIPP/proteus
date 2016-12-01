807e349c1df12e161b6b753659f0e47224a7737a
Moritz Kiehn <msmk@cern.ch>
2016-12-01 09:50:21 +0100

    analyzers/matching: boyscouting

--- src/analyzers/matching.cpp
+++ src/analyzers/matching.cpp
@@ -1,98 +1,94 @@
 #include "matching.h"
 
 #include <cassert>
-#include <sstream>
-#include <math.h>
 #include <iomanip>
+#include <iostream>
+#include <math.h>
+#include <sstream>
 
 #include <TDirectory.h>
-#include <TH2D.h>
-#include <TH1D.h>
 #include <TF1.h>
+#include <TH1D.h>
+#include <TH2D.h>
 
-// Access to the device being analyzed and its sensors
-#include "../mechanics/device.h"
-#include "../mechanics/sensor.h"
-// Access to the data stored in the event
-#include "../storage/hit.h"
-#include "../storage/cluster.h"
-#include "../storage/plane.h"
-#include "../storage/track.h"
-#include "../storage/event.h"
-// Some generic processors to calcualte typical event related things
-#include "../processors/processors.h"
-
-#include <iostream>
-#include <iomanip>
-
+#include "mechanics/device.h"
+#include "mechanics/sensor.h"
+#include "processors/processors.h"
+#include "storage/cluster.h"
+#include "storage/event.h"
+#include "storage/hit.h"
+#include "storage/plane.h"
+#include "storage/track.h"
 
 //=========================================================
 Analyzers::Matching::Matching(const Mechanics::Device* refDevice,
-                   const Mechanics::Device* dutDevice,
-                   TDirectory* dir,
-                   const char* suffix,
-                   double pixelExtension,
-                   double maxMatchingDist,
-                   double sigmaBins,
-                   unsigned int pixBinsX,
-                   unsigned int pixBinsY) :
-  DualAnalyzer(refDevice, dutDevice, dir, suffix),
-  _plotDir(0)
+                              const Mechanics::Device* dutDevice,
+                              TDirectory* dir,
+                              const char* suffix,
+                              double pixelExtension,
+                              double maxMatchingDist,
+                              double sigmaBins,
+                              unsigned int pixBinsX,
+                              unsigned int pixBinsY)
+    : DualAnalyzer(refDevice, dutDevice, dir, suffix), _plotDir(0)
 {
-  assert(refDevice && dutDevice && "Analyzer: can't initialize with null device");
+  assert(refDevice && dutDevice &&
+         "Analyzer: can't initialize with null device");
 
   // Makes or gets a directory called from inside _dir with this name
   _plotDir = makeGetDirectory("Matching");
 
-  std::stringstream name; // Build name strings for each histo
+  std::stringstream name;  // Build name strings for each histo
   std::stringstream title; // Build title strings for each histo
 
   unsigned int numBins = maxMatchingDist * sigmaBins;
-  if (!numBins) numBins = 1;
+  if (!numBins)
+    numBins = 1;
 
   // Generate a histogram for each sensor in the device
-  for (unsigned int nsens=0; nsens<_dutDevice->getNumSensors(); nsens++)
-  {
+  for (unsigned int nsens = 0; nsens < _dutDevice->getNumSensors(); nsens++) {
     const Mechanics::Sensor* sensor = _dutDevice->getSensor(nsens);
 
-    name.str(""); title.str("");
-    name << sensor->getName()
-         << "Distance" << _nameSuffix;
-    title << sensor->getName()
-          << " Matched Cluster Distance"
+    name.str("");
+    title.str("");
+    name << sensor->getName() << "Distance" << _nameSuffix;
+    title << sensor->getName() << " Matched Cluster Distance"
           << ";Radial distance [standard deviation]"
-          << ";Tracks / " << std::setprecision(2) << maxMatchingDist / (double)numBins << " standard deviation";
-    TH1D* matchDist = new TH1D(name.str().c_str(), title.str().c_str(),
-                               numBins, 0, maxMatchingDist);
+          << ";Tracks / " << std::setprecision(2)
+          << maxMatchingDist / (double)numBins << " standard deviation";
+    TH1D* matchDist = new TH1D(name.str().c_str(), title.str().c_str(), numBins,
+                               0, maxMatchingDist);
     matchDist->SetDirectory(_plotDir);
     _matchDist.push_back(matchDist);
 
-    const double maxDistX = maxMatchingDist *
-                            sqrt( pow(sensor->getPitchX() / sqrt(12.0), 2) +
-                                  pow(_refDevice->getSensor(0)->getPitchX() / sqrt(12.0), 2));
-    const double maxDistY = maxMatchingDist *
-                            sqrt( pow(sensor->getPitchY() / sqrt(12.0), 2) +
-                                  pow(_refDevice->getSensor(0)->getPitchY() / sqrt(12.0), 2));
+    const double maxDistX =
+        maxMatchingDist *
+        sqrt(pow(sensor->getPitchX() / sqrt(12.0), 2) +
+             pow(_refDevice->getSensor(0)->getPitchX() / sqrt(12.0), 2));
+    const double maxDistY =
+        maxMatchingDist *
+        sqrt(pow(sensor->getPitchY() / sqrt(12.0), 2) +
+             pow(_refDevice->getSensor(0)->getPitchY() / sqrt(12.0), 2));
 
-    name.str(""); title.str("");
-    name << sensor->getName()
-         << "DistanceX" << _nameSuffix;
-    title << sensor->getName()
-          << " Matched Cluster Distance"
+    name.str("");
+    title.str("");
+    name << sensor->getName() << "DistanceX" << _nameSuffix;
+    title << sensor->getName() << " Matched Cluster Distance"
           << ";Distance in X [" << _dutDevice->getSpaceUnit() << "]"
-          << ";Tracks / " << std::setprecision(2) << 2 * maxDistX / (2 * numBins) << " " << _dutDevice->getSpaceUnit();
+          << ";Tracks / " << std::setprecision(2)
+          << 2 * maxDistX / (2 * numBins) << " " << _dutDevice->getSpaceUnit();
     TH1D* matchDistX = new TH1D(name.str().c_str(), title.str().c_str(),
                                 2 * numBins, -maxDistX, maxDistX);
     matchDistX->SetDirectory(_plotDir);
     _matchDistX.push_back(matchDistX);
 
-    name.str(""); title.str("");
-    name << sensor->getName()
-         << "DistanceY" << _nameSuffix;
-    title << sensor->getName()
-          << " Matched Cluster Distance"
+    name.str("");
+    title.str("");
+    name << sensor->getName() << "DistanceY" << _nameSuffix;
+    title << sensor->getName() << " Matched Cluster Distance"
           << ";Distance in Y [" << _dutDevice->getSpaceUnit() << "]"
-          << ";Tracks / " << std::setprecision(2) << 2 * maxDistY / (2 * numBins) << " " << _dutDevice->getSpaceUnit();
+          << ";Tracks / " << std::setprecision(2)
+          << 2 * maxDistY / (2 * numBins) << " " << _dutDevice->getSpaceUnit();
     TH1D* matchDistY = new TH1D(name.str().c_str(), title.str().c_str(),
                                 2 * numBins, -maxDistY, maxDistY);
     matchDistY->SetDirectory(_plotDir);
@@ -104,31 +100,29 @@ Analyzers::Matching::Matching(const Mechanics::Device* refDevice,
     const double pixHalfWidth = sensor->getPitchX() / 2.0 * pixelExtension;
     const double pixHalfHeight = sensor->getPitchY() / 2.0 * pixelExtension;
 
-    name.str(""); title.str("");
-    name << sensor->getName()
-         <<  "InPixelTracks" << _nameSuffix;
-    title << sensor->getName()
-          << " In Pixel Track Occupancy"
+    name.str("");
+    title.str("");
+    name << sensor->getName() << "InPixelTracks" << _nameSuffix;
+    title << sensor->getName() << " In Pixel Track Occupancy"
           << ";X position [" << _dutDevice->getSpaceUnit() << "]"
           << ";Y position [" << _dutDevice->getSpaceUnit() << "]"
           << ";Tracks";
-    TH2D* inPixTracks = new TH2D(name.str().c_str(), title.str().c_str(),
-                                 binsX, -pixHalfWidth, pixHalfWidth,
-                                 binsY, -pixHalfHeight, pixHalfHeight);
+    TH2D* inPixTracks =
+        new TH2D(name.str().c_str(), title.str().c_str(), binsX, -pixHalfWidth,
+                 pixHalfWidth, binsY, -pixHalfHeight, pixHalfHeight);
     inPixTracks->SetDirectory(_plotDir);
     _inPixelTracks.push_back(inPixTracks);
 
-    name.str(""); title.str("");
-    name << sensor->getName()
-         <<  "InPixelTot" << _nameSuffix;
-    title << sensor->getName()
-          << " In Pixel Average Cluster ToT"
+    name.str("");
+    title.str("");
+    name << sensor->getName() << "InPixelTot" << _nameSuffix;
+    title << sensor->getName() << " In Pixel Average Cluster ToT"
           << ";X position [" << _dutDevice->getSpaceUnit() << "]"
           << ";Y position [" << _dutDevice->getSpaceUnit() << "]"
           << ";Average Cluster ToT";
-    TH2D* inPixTot = new TH2D(name.str().c_str(), title.str().c_str(),
-                              binsX, -pixHalfWidth, pixHalfWidth,
-                              binsY, -pixHalfHeight, pixHalfHeight);
+    TH2D* inPixTot =
+        new TH2D(name.str().c_str(), title.str().c_str(), binsX, -pixHalfWidth,
+                 pixHalfWidth, binsY, -pixHalfHeight, pixHalfHeight);
     inPixTot->SetDirectory(_plotDir);
     _inPixelTot.push_back(inPixTot);
   }
@@ -136,7 +130,7 @@ Analyzers::Matching::Matching(const Mechanics::Device* refDevice,
 
 //=========================================================
 void Analyzers::Matching::processEvent(const Storage::Event* refEvent,
-				       const Storage::Event* dutEvent)
+                                       const Storage::Event* dutEvent)
 {
   assert(refEvent && dutEvent && "Analyzer: can't process null events");
 
@@ -148,9 +142,9 @@ void Analyzers::Matching::processEvent(const Storage::Event* refEvent,
     return;
 
   /** Loop in all reconstructed tracls*/
-  for(unsigned int ntrack=0; ntrack<refEvent->getNumTracks(); ntrack++) {
+  for (unsigned int ntrack = 0; ntrack < refEvent->getNumTracks(); ntrack++) {
     const Storage::Track* track = refEvent->getTrack(ntrack);
-    
+
     // Check if the track passes the cuts
     if (!checkCuts(track))
       continue;
@@ -162,7 +156,8 @@ void Analyzers::Matching::processEvent(const Storage::Event* refEvent,
 
     // Get the matches from the track
     // NOTE 2016-11-15 msmk: disabled for now due to incompatible api changes
-    // for(unsigned int nmatch=0; nmatch<track->getNumMatchedClusters(); nmatch++){
+    // for(unsigned int nmatch=0; nmatch<track->getNumMatchedClusters();
+    // nmatch++){
     //   Storage::Cluster* cluster = track->getMatchedCluster(nmatch);
     //
     //   // Check if this cluster passes the cuts
@@ -172,11 +167,12 @@ void Analyzers::Matching::processEvent(const Storage::Event* refEvent,
     //   matches.at(cluster->getPlane()->sensorId()) = cluster;
     // }
 
-    for (unsigned int nsens=0; nsens<_dutDevice->getNumSensors(); nsens++){
+    for (unsigned int nsens = 0; nsens < _dutDevice->getNumSensors(); nsens++) {
       const Mechanics::Sensor* sensor = _dutDevice->getSensor(nsens);
       Storage::Cluster* match = matches.at(nsens);
 
-      if (!match) continue;
+      if (!match)
+        continue;
 
       _matchDist.at(nsens)->Fill(match->getMatchDistance());
 
@@ -193,8 +189,7 @@ void Analyzers::Matching::processEvent(const Storage::Event* refEvent,
       sensor->spaceToPixel(tx, ty, tz, px, py);
 
       // Find the distance of this track to all pixels in its matched cluster
-      for (unsigned int nhit = 0; nhit < match->getNumHits(); nhit++)
-      {
+      for (unsigned int nhit = 0; nhit < match->getNumHits(); nhit++) {
         const Storage::Hit* hit = match->getHit(nhit);
         const double hitX = hit->getPixX() + 0.5;
         const double hitY = hit->getPixY() + 0.5;
@@ -206,15 +201,14 @@ void Analyzers::Matching::processEvent(const Storage::Event* refEvent,
         _inPixelTot.at(nsens)->Fill(pixDistX, pixDistY, hit->getValue());
       }
     } // loop in sensors
-    
+
   } // end loop in tracks
 }
 
-  //=========================================================
+//=========================================================
 void Analyzers::Matching::postProcessing()
 {
-  for (unsigned int nsens = 0; nsens < _dutDevice->getNumSensors(); nsens++)
-  {
+  for (unsigned int nsens = 0; nsens < _dutDevice->getNumSensors(); nsens++) {
     const Mechanics::Sensor* sensor = _dutDevice->getSensor(nsens);
 
     TH2D* tot = _inPixelTot.at(nsens);
@@ -224,32 +218,28 @@ void Analyzers::Matching::postProcessing()
            hits->GetNbinsY() == tot->GetNbinsY() &&
            "Matching: histograms should have the same number of bins");
 
-    for (int nx = 1; nx <= tot->GetNbinsX(); nx++)
-    {
-      for (int ny = 1; ny <= tot->GetNbinsY(); ny++)
-      {
+    for (int nx = 1; nx <= tot->GetNbinsX(); nx++) {
+      for (int ny = 1; ny <= tot->GetNbinsY(); ny++) {
         const unsigned int num = hits->GetBinContent(nx, ny);
-        if (num == 0) continue;
+        if (num == 0)
+          continue;
         const double average = tot->GetBinContent(nx, ny) / (double)num;
         tot->SetBinContent(nx, ny, average);
       }
     }
 
-    for (unsigned int axis = 0; axis < 2; axis++)
-    {
-      TH1D* dist = axis ? _matchDistX.at(nsens) :
-                          _matchDistY.at(nsens);
+    for (unsigned int axis = 0; axis < 2; axis++) {
+      TH1D* dist = axis ? _matchDistX.at(nsens) : _matchDistY.at(nsens);
 
-      double pixelWidth = axis ? sensor->getPitchX() :
-                                 sensor->getPitchY();
-      double beamSigma = axis ? _refDevice->getSensor(0)->getPitchX() :
-                                _refDevice->getSensor(0)->getPitchY();
+      double pixelWidth = axis ? sensor->getPitchX() : sensor->getPitchY();
+      double beamSigma = axis ? _refDevice->getSensor(0)->getPitchX()
+                              : _refDevice->getSensor(0)->getPitchY();
 
       TF1* fit = Processors::fitPixelBeam(dist, pixelWidth, beamSigma, false);
 
       std::stringstream ss;
       ss << _dutDevice->getName() << sensor->getName() << "PixelBeamFit"
-         << ( axis ? "X" : "Y") << _nameSuffix;
+         << (axis ? "X" : "Y") << _nameSuffix;
       _plotDir->WriteObject(fit, ss.str().c_str());
     }
   }
