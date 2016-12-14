49268dac035b1909ca3384da2c1a3725c058872e
Moritz Kiehn <msmk@cern.ch>
2016-12-14 17:02:36 +0100

    test: use combined configuration file

--- test/run_dut0_noisescan.sh
+++ test/run_dut0_noisescan.sh
@@ -9,6 +9,7 @@ rundir=$(printf "run%06d" $RUN)
 rawfile=$(printf "raw/run%06d.root" $RUN)
 prefix=$(printf "output/run%06d-" $RUN)
 
-pt-noisescan $FLAGS -d ${rundir}/device.toml \
-  -c ${rundir}/configs/dut0_noisescan.toml \
+pt-noisescan $FLAGS \
+  -d ${rundir}/device.toml \
+  -c ${rundir}/noisescan.toml -u dut0 \
   ${rawfile} ${prefix}dut0_noisescan
--- test/run_tel_noisescan.sh
+++ test/run_tel_noisescan.sh
@@ -9,6 +9,7 @@ rundir=$(printf "run%06d" $RUN)
 rawfile=$(printf "raw/run%06d.root" $RUN)
 prefix=$(printf "output/run%06d-" $RUN)
 
-pt-noisescan $FLAGS -d ${rundir}/device.toml \
-  -c ${rundir}/configs/tel_noisescan.toml \
+pt-noisescan $FLAGS \
+  -d ${rundir}/device.toml \
+  -c ${rundir}/noisescan.toml -u tel \
   ${rawfile} ${prefix}tel_noisescan
