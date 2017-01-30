a907e4288da863ebe244571a69cecf8134baa214
Moritz Kiehn <msmk@cern.ch>
2017-01-30 10:51:43 +0100

    cmake: only require c++11 but consider ROOT compiler flags

---
+++ cmake/RequireCXX11.cmake
@@ -0,0 +1,45 @@
+# Require support for C++11 if no standard has been set yet.
+#
+# Copyright 2015-2017 Moritz Kiehn <msmk@cern.ch>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+include(CheckCXXCompilerFlag)
+
+CHECK_CXX_COMPILER_FLAG("-std=c++11" _has_cxx11_support)
+if(NOT _has_cxx11_support)
+  message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support.")
+endif()
+
+# check for existing compiler flags
+string(REGEX MATCH "-std=[a-z0-9+]+" _defined_std ${CMAKE_CXX_FLAGS})
+if(_defined_std MATCHES "-std=.+")
+  message(STATUS "C++ standard compiler flag ${_defined_std} already defined.")
+  return()
+endif()
+
+# enable support
+if(CMAKE_VERSION VERSION_LESS "3.1")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+  message(STATUS "Added -std=c++11 compiler flag.")
+else()
+  set(CMAKE_CXX_STANDARD 11)
+  set(CMAKE_CXX_STANDARD_REQUIRED ON)
+  message(STATUS "Require CXX_STANDARD=11.")
+endif()
