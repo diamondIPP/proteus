de31fa0d28847f7c6bccdf2b1e50ae2ddf08eccd
Moritz Kiehn <msmk@cern.ch>
2017-01-30 15:30:34 +0100

    application: update utils::Arguments usage

--- src/application.cpp
+++ src/application.cpp
@@ -25,48 +25,48 @@ Application::Application(const std::string& name,
 void Application::initialize(int argc, char const* argv[])
 {
   Utils::Arguments args(m_desc);
-  args.addOption('d', "device", "device configuration file", "device.toml");
-  args.addOption('g', "geometry", "load a separate geometry file");
-  args.addOption('c', "config", "analysis configuration file", "analysis.toml");
-  args.addOption('u', "subsection", "use the given configuration sub-section");
-  args.addOption('s', "skip_events", "skip the first n events", 0);
-  args.addOption('n', "num_events", "number of events to process", -1);
-  args.addRequiredArgument("input", "path to the input file");
-  args.addRequiredArgument("output_prefix", "output path prefix");
+  args.addOptional('d', "device", "device configuration file", "device.toml");
+  args.addOptional('g', "geometry", "load a separate geometry file");
+  args.addOptional('c', "config", "analysis configuration file", "analysis.toml");
+  args.addOptional('u', "subsection", "use the given configuration sub-section");
+  args.addOptional('s', "skip_events", "skip the first n events", 0);
+  args.addOptional('n', "num_events", "number of events to process", -1);
+  args.addRequired("input", "path to the input file");
+  args.addRequired("output_prefix", "output path prefix");
 
   // should print help automatically
   if (args.parse(argc, argv))
     std::exit(EXIT_FAILURE);
 
   // TODO 2016-12-13 msmk: add command line flag
-  Utils::Logger::setGlobalLevel(Utils::Logger::Level::Info);
+  Utils::Logger::setGlobalLevel(Utils::Logger::Level::Debug);
 
+  // select configuration section
+  std::string section = m_name;
+  if (args.has("subsection")) {
+    section += '.';
+    section += args.get("subsection");
+  }
   // read configuration w/ automatic handling of defaults
-  auto pathCfg = args.option<std::string>("config");
-  auto subsection = args.option<std::string>("subsection");
-  std::string section =
-      (subsection.empty() ? m_name : (m_name + '.' + subsection));
-  auto cfgFull = Utils::Config::readConfig(pathCfg);
-  // select configuration subsection
-  const toml::Value* cfgTool = cfgFull.find(section);
-  if (!cfgTool)
-    FAIL("configuration section '", m_name, "' is missing");
-  m_cfg = Utils::Config::withDefaults(*cfgTool, m_cfg);
-  INFO("read configuration '", section, "' from '", pathCfg, "'");
+  const toml::Value cfgAll = Utils::Config::readConfig(args.get("config"));
+  const toml::Value* cfg = cfgAll.find(section);
+  if (!cfg)
+    FAIL("configuration section '", section, "' is missing");
+  m_cfg = Utils::Config::withDefaults(*cfg, m_cfg);
+  INFO("read configuration '", section, "' from '", args.get("config"), "'");
 
   // read device w/ optional geometry override
-  auto pathDev = args.option<std::string>("device");
-  auto pathGeo = args.option<std::string>("geometry");
-  m_dev.reset(new Mechanics::Device(Mechanics::Device::fromFile(pathDev)));
-  if (!pathGeo.empty())
-    m_dev->setGeometry(Mechanics::Geometry::fromFile(pathGeo));
+  m_dev.reset(
+      new Mechanics::Device(Mechanics::Device::fromFile(args.get("device"))));
+  if (args.has("geometry"))
+    m_dev->setGeometry(Mechanics::Geometry::fromFile(args.get("geometry")));
 
   // setup input and i/o settings
-  m_input.reset(new Storage::StorageIO(args.argument<std::string>(0),
-                                       Storage::INPUT, m_dev->numSensors()));
-  m_outputPrefix = args.argument<std::string>(1);
-  m_skipEvents = args.option<uint64_t>("skip_events");
-  m_numEvents = args.option<uint64_t>("num_events");
+  m_input.reset(new Storage::StorageIO(args.get("input"), Storage::INPUT,
+                                       m_dev->numSensors()));
+  m_outputPrefix = args.get("output_prefix");
+  m_skipEvents = args.get<uint64_t>("skip_events");
+  m_numEvents = args.get<uint64_t>("num_events");
 }
 
 std::string Application::outputPath(const std::string& name) const
