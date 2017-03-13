4b97ba9a8972d426652b5d514343e25b0f2ccece
Moritz Kiehn <msmk@cern.ch>
2017-03-13 22:37:22 +0000

    utils/densemask: fix limit calculations

--- src/utils/densemask.cpp
+++ src/utils/densemask.cpp
@@ -11,9 +11,9 @@
 // create an empty mask, i.e. no masked pixels, for the given area
 Utils::DenseMask::DenseMask(int col0, int row0, int sizeCol, int sizeRow)
     : m_col0(col0)
+    , m_col1(col0 + sizeCol)
     , m_row0(row0)
-    , m_sizeCol(sizeCol)
-    , m_sizeRow(sizeRow)
+    , m_row1(row0 + sizeRow)
     , m_mask(sizeCol * sizeRow, false)
 {
   assert(0 <= sizeCol);
@@ -24,19 +24,19 @@ Utils::DenseMask::DenseMask() : DenseMask(0, 0, 0, 0) {}
 
 Utils::DenseMask::DenseMask(const std::set<ColumnRow>& masked)
     : m_col0(std::numeric_limits<int>::max())
+    , m_col1(std::numeric_limits<int>::min())
     , m_row0(std::numeric_limits<int>::max())
-    , m_sizeCol(0)
-    , m_sizeRow(0)
+    , m_row1(std::numeric_limits<int>::min())
 {
   // first iteration to get boundaries
   for (const auto& pos : masked) {
     m_col0 = std::min<int>(m_col0, std::get<0>(pos));
+    m_col1 = std::max<int>(m_col1, std::get<0>(pos) + 1);
     m_row0 = std::min<int>(m_row0, std::get<1>(pos));
-    m_sizeCol = std::max<int>(m_sizeCol, std::get<0>(pos) - m_col0 + 1);
-    m_sizeRow = std::max<int>(m_sizeRow, std::get<1>(pos) - m_row0 + 1);
+    m_row1 = std::max<int>(m_row1, std::get<1>(pos) + 1);
   }
   // second iteration to fill the mask
-  m_mask.resize(m_sizeCol * m_sizeRow, false);
+  m_mask.resize((m_col1 - m_col0) * (m_row1 - m_row0), false);
   for (const auto& pos : masked)
     m_mask[index(std::get<0>(pos), std::get<1>(pos))] = true;
 }
@@ -45,11 +45,12 @@ Utils::DenseMask Utils::DenseMask::protruded(int offset) const
 {
   assert(0 <= offset);
 
-  DenseMask bigger(m_col0 - offset, m_row0 - offset, m_sizeCol + 2 * offset,
-                   m_sizeRow + 2 * offset);
+  int newSizeCol = (m_col1 - m_col0) + 2 * offset;
+  int newSizeRow = (m_row1 - m_row0) + 2 * offset;
+  DenseMask bigger(m_col0 - offset, m_row0 - offset, newSizeCol, newSizeRow);
 
-  for (int c = m_col0; c < (m_col0 + m_sizeCol); ++c) {
-    for (int r = m_row0; r < (m_row0 + m_sizeRow); ++r) {
+  for (int c = m_col0; c < m_col1; ++c) {
+    for (int r = m_row0; r < m_row1; ++r) {
       if (isMasked(c, r)) {
         // set rectangular area around the masked pixel w/ the given offset
         for (int mc = c - offset; mc <= (c + offset); ++mc) {
@@ -67,26 +68,26 @@ std::ostream& Utils::operator<<(std::ostream& os, const DenseMask& pm)
 {
   // print header line w/ column numbers
   os << "     ";
-  for (int c = pm.m_col0; c < (pm.m_col0 + pm.m_sizeCol); c += 5)
+  for (int c = pm.m_col0; c < pm.m_col1; c += 5)
     os << std::setw(4) << std::left << c << ' ';
   os << "\n    +";
-  for (int c = pm.m_col0; c < (pm.m_col0 + pm.m_sizeCol); ++c)
+  for (int c = pm.m_col0; c < pm.m_col1; ++c)
     os << '-';
   os << "+\n";
   // print each row starting w/ the row number
-  for (int r = (pm.m_row0 + pm.m_sizeRow - 1); pm.m_row0 <= r; --r) {
+  for (int r = (pm.m_row1 - 1); pm.m_row0 <= r; --r) {
     os << std::setw(4) << std::right << r << '|';
-    for (int c = pm.m_col0; c < (pm.m_col0 + pm.m_sizeCol); ++c) {
+    for (int c = pm.m_col0; c < pm.m_col1; ++c) {
       os << (pm.isMasked(c, r) ? 'X' : ' ');
     }
     os << "|\n";
   }
   // print bottom line w/ column numbers
   os << "    +";
-  for (int c = pm.m_col0; c < (pm.m_col0 + pm.m_sizeCol); ++c)
+  for (int c = pm.m_col0; c < pm.m_col1; ++c)
     os << '-';
   os << "+\n     ";
-  for (int c = pm.m_col0; c < (pm.m_col0 + pm.m_sizeCol); c += 5)
+  for (int c = pm.m_col0; c < pm.m_col1; c += 5)
     os << std::setw(4) << std::left << c << ' ';
   return os;
 }
