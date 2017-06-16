b33af411a8968950b354ca48f40f50836421338f
Moritz Kiehn <msmk@cern.ch>
2017-06-16 15:06:41 +0200

    progressbar: use processed count directly and add time-based redraw

--- src/utils/progressbar.h
+++ src/utils/progressbar.h
@@ -7,65 +7,63 @@
 #ifndef PT_PROGRESSBAR_H
 #define PT_PROGRESSBAR_H
 
+#include <chrono>
+#include <cstdlib>
 #include <iomanip>
 #include <iostream>
+#include <limits>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
 namespace Utils {
 
-/** Display an updatable progress bar on a single output line */
+/** Display a progress bar on a single output line */
 class ProgressBar {
 public:
-  /** Construct with a fixed output line length.
-   *
-   * \param lineLength  Total length of the output line
-   */
+  /** Construct with the line length of the terminal. */
+  ProgressBar() : ProgressBar(queryLineLength()) {}
+  /** Construct with a fixed line length. */
   ProgressBar(int lineLength)
       : m_os(std::cout)
-      , m_curr(-1)
-      , m_max((9 < lineLength) ? (lineLength - 9) : 0)
+      , m_lastUpdate(std::chrono::steady_clock::now())
+      , m_length(std::max(lineLength - 8, 0))
   {
   }
-  /** Construct with using the line length of the terminal. */
-  ProgressBar() : ProgressBar(queryLineLength()) {}
 
-  /** Update the progress bar if necessary. Fraction must be in [0,1]. */
-  void update(float fraction)
+  /** Update the progress bar if necessary.
+   *
+   * \param processed  Number of processed items, must be in [0, total]
+   * \param total      Number of total items
+   */
+  template <typename I, typename = typename std::is_integral<I>::type>
+  void update(I processed, I total = std::numeric_limits<I>::max())
   {
-    int status = fraction * m_max;
-    // only update if neccessary
-    if (m_curr < status) {
-      m_curr = status;
-      display();
-    }
+    using std::chrono::steady_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::milliseconds;
+
+    // time-based redraw throttling w/ 60fps
+    auto now = steady_clock::now();
+    if (duration_cast<milliseconds>(now - m_lastUpdate) < milliseconds(16))
+      return;
+
+    drawBar(processed, total);
+    m_lastUpdate = now;
   }
   /** Overwrite the progress bar with empty spaces. */
   void clear()
   {
-    for (int i = 0; i <= (m_max + 8); ++i)
+    for (int i = 0; i < (m_length + 8); ++i)
       m_os << ' ';
     m_os << '\r' << std::flush;
   }
 
 private:
-  /** Display the current progress status. */
-  void display()
-  {
-    m_os << "[";
-    for (int i = 0; i < m_curr; ++i)
-      m_os << '=';
-    for (int i = m_curr; i <= m_max; ++i)
-      m_os << ' ';
-    // after printing rewind back to the beginning of the line so that the
-    // next update (or unrelated messages) overwrite the current status
-    int percent = static_cast<int>(100.0f * m_curr / m_max);
-    m_os << "] " << std::setw(3) << percent << "%\r" << std::flush;
-  }
-
   /** Query the connected terminal for its line length. */
-  static int queryLineLength() {
-    // from https://www.linuxquestions.org/questions/programming-9/get-width-height-of-a-terminal-window-in-c-810739/
+  static int queryLineLength()
+  {
+// from
+// https://www.linuxquestions.org/questions/programming-9/get-width-height-of-a-terminal-window-in-c-810739/
 #ifdef TIOCGSIZE
     struct ttysize ts;
     ioctl(STDIN_FILENO, TIOCGSIZE, &ts);
@@ -74,14 +72,30 @@ private:
     struct winsize ts;
     ioctl(STDIN_FILENO, TIOCGWINSZ, &ts);
     return ts.ws_col;
-# else
-    return 60; // fallback
+#else
+    return 50; // fallback
 #endif /* TIOCGSIZE */
   }
 
+  template <typename I, typename = typename std::is_integral<I>::type>
+  void drawBar(I current, I total)
+  {
+    int bars = (m_length * current) / total;
+    int percent = (100 * current) / total;
+
+    m_os << "[";
+    for (int i = 0; i < bars; ++i)
+      m_os << '=';
+    for (int i = bars; i < m_length; ++i)
+      m_os << ' ';
+    // after printing rewind back to the beginning of the line so that the
+    // next update (or unrelated messages) can overwrite the current status
+    m_os << "] " << std::setw(3) << percent << "%\r" << std::flush;
+  }
+
   std::ostream& m_os;
-  int m_curr;
-  int m_max;
+  std::chrono::steady_clock::time_point m_lastUpdate;
+  int m_length;
 };
 
 } // namespace Utils
