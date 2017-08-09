fbfe76ed30572c1a1612d1841c295f1a069e832b
Moritz Kiehn <msmk@cern.ch>
2017-08-09 14:02:22 +0200

    eventprinter: use logger

--- lib/analyzers/eventprinter.cpp
+++ lib/analyzers/eventprinter.cpp
@@ -3,13 +3,16 @@
 #include <iostream>
 
 #include "storage/event.h"
+#include "utils/logger.h"
+
+PT_SETUP_LOCAL_LOGGER(EventPrinter)
 
 std::string Analyzers::EventPrinter::name() const { return "EventPrinter"; }
 
 void Analyzers::EventPrinter::analyze(const Storage::Event& event)
 {
-  std::cout << "event:\n";
-  event.print(std::cout, "  ");
+  logger().debug("event:\n");
+  logger().debugp(event, "  ");
 }
 
 void Analyzers::EventPrinter::finalize() {}
