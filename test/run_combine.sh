e4ff44eaf2b99acc872a422356ec689c9a4a1916
Moritz Kiehn <msmk@cern.ch>
2017-10-21 15:16:08 +0200

    test: fix paths and configs for combine

--- test/run_combine.sh
+++ test/run_combine.sh
@@ -2,7 +2,7 @@
 #
 # combine two files into a single stream
 
-pt-convert -s 1000 -n 2000 \
+pt-combine -s 1000 -n 2000 \
   output/merged0.root raw/run000875.root raw/run001066.root
-pt-convert -s 3000 -n 2000 -u example1 \
+pt-combine -s 3000 -n 2000 -u example1 \
   output/merged1.root raw/run001066.root raw/run000875.root
