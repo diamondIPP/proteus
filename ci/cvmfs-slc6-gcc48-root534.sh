665049765a13de7271a7fe1429a60fe51d7fe8a3
Moritz Kiehn <msmk@cern.ch>
2017-11-06 17:45:29 +0100

    ci: remove obsolete setup scripts

--- ci/cvmfs-slc6-gcc48-root534.sh
+++
@@ -1,8 +0,0 @@
-# setup CVMFS-based environment w/ gcc 4.8 and ROOT 5.34
-
-source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.8.4/x86_64-slc6/setup.sh
-pushd .
-cd  /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.36/x86_64-slc6-gcc48-opt/root
-source bin/thisroot.sh
-popd
-export PATH="/cvmfs/sft.cern.ch/lcg/contrib/CMake/3.2.3/Linux-x86_64/bin:${PATH}"
