2f5fa4ea0bca4890f9cc9541d1f4619154c730a4
Moritz Kiehn <msmk@cern.ch>
2018-06-25 23:41:50 +0200

    test: update config check script

--- test/check_configs.sh
+++ test/check_configs.sh
@@ -1,28 +1,30 @@
-#!/bin/sh -ex
+#!/bin/sh
 #
 # check validity of config files
 
-echo "using $(which pt-cfg)"
+set -ex
 
-# pt-cfg should succeed
-pt-cfg run000875/device.toml
-pt-cfg run000875/geometry/tel_aligned.toml
-pt-cfg run000875/geometry/unaligned.toml
-pt-cfg run001066/device.toml
-pt-cfg run001066/geometry/aligned.toml
-pt-cfg run001066/geometry/tel_aligned.toml
-pt-cfg run001066/geometry/unaligned.toml
-pt-cfg run001066/masks/duts_mask_empty.toml
-pt-cfg run001066/masks/tel_mask_empty.toml
+echo "=== using $(which pt-cfg)"
+
+# pt-cfg should succeed for regular configs
+pt-cfg device.toml
+pt-cfg geometry/unigetel_ebeam012_nparticles01.toml
+pt-cfg geometry/unigetel_ebeam012_nparticles01-telescope.toml
+pt-cfg geometry/unigetel_ebeam120_nparticles01.toml
+pt-cfg geometry/unigetel_ebeam120_nparticles01-telescope.toml
+pt-cfg geometry/unigetel_ebeam120_nparticles01_misalignxyrotz.toml
+pt-cfg geometry/unigetel_ebeam120_nparticles01_misalignxyrotz-telescope.toml
+pt-cfg masks/telescope_empty.toml
+pt-cfg masks/dut_empty.toml
 
 # pt-cfg should fail
 # NOTE the shell seems to treat `! <cmd>` as a single command. To ensure the
 # that the shell exits when pt-cfg unexpectedly succeeds, an additional
 # failing command `false` is needed.
-! pt-cfg run000875/ || false # directory
-! pt-cfg run000875/does-not-exists || false
+! pt-cfg geometry/ || false # directory
+! pt-cfg geometry/does-not-exists || false
 
-! pt-cfg geometry/missing_id.toml || false
-! pt-cfg geometry/non_orthogonal.toml || false
-pt-cfg geometry/with_angles.toml
-pt-cfg geometry/with_directions.toml
+! pt-cfg geometry/test_failure-missing_id.toml || false
+! pt-cfg geometry/test_failure-non_orthogonal.toml || false
+pt-cfg geometry/test_success-with_angles.toml
+pt-cfg geometry/test_success-with_directions.toml
