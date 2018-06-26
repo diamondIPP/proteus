cc34160df8ca3d0fd0682b3eaa1c9a1fc470f5c6
Moritz Kiehn <msmk@cern.ch>
2018-06-26 02:11:58 +0200

    test: replace combine check script

---
+++ test/check_combine.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#
+# check pt-combine functionality
+
+set -ex
+
+flags=$@ # e.g. --no-progress for ci-jobs
+
+pt-combine ${flags} -n 10000 \
+    output/merged0.root \
+    data/unigetel_ebeam012_nparticles01.root \
+    data/unigetel_ebeam012_nparticles01.root
+
+./root-checker - output/merged0.root <<__END_OF_CONFIG__
+  Event        entries 10000
+  Plane0/Hits  entries 10000
+  Plane1/Hits  entries 10000
+  Plane2/Hits  entries 10000
+  Plane3/Hits  entries 10000
+  Plane4/Hits  entries 10000
+  Plane5/Hits  entries 10000
+  Plane6/Hits  entries 10000
+  Plane7/Hits  entries 10000
+  Plane8/Hits  entries 10000
+  Plane9/Hits  entries 10000
+  Plane10/Hits entries 10000
+  Plane11/Hits entries 10000
+  Plane12/Hits entries 10000
+  Plane13/Hits entries 10000
+__END_OF_CONFIG__
+
+pt-combine ${flags} -n 10000 \
+    output/merged1.root \
+    data/unigetel_ebeam120_nparticles02.root \
+    data/unigetel_ebeam120_nparticles02.root
+
+./root-checker - output/merged1.root <<__END_OF_CONFIG__
+  Event        entries 10000
+  Plane0/Hits  entries 10000
+  Plane1/Hits  entries 10000
+  Plane2/Hits  entries 10000
+  Plane3/Hits  entries 10000
+  Plane4/Hits  entries 10000
+  Plane5/Hits  entries 10000
+  Plane6/Hits  entries 10000
+  Plane7/Hits  entries 10000
+  Plane8/Hits  entries 10000
+  Plane9/Hits  entries 10000
+  Plane10/Hits entries 10000
+  Plane11/Hits entries 10000
+  Plane12/Hits entries 10000
+  Plane13/Hits entries 10000
+__END_OF_CONFIG__
