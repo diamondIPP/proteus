0d15e61a57b3181a18869e2f4a162a865ef36171
Moritz Kiehn <msmk@cern.ch>
2018-09-17 19:46:17 +0200

    definitions: clarify naming and parameters

--- lib/utils/definitions.h
+++ lib/utils/definitions.h
@@ -21,26 +21,33 @@ constexpr Index kInvalidIndex = static_cast<Index>(-1);
 // Digital matrix position defined by column and row index
 typedef std::pair<Index, Index> ColumnRow;
 
-// commonly used vector and matrix types
-typedef ROOT::Math::SMatrix<double, 2, 2> Matrix2;
-typedef ROOT::Math::SMatrix<double, 2, 3> Matrix23;
-typedef ROOT::Math::SMatrix<double, 2, 6> Matrix26;
-typedef ROOT::Math::SMatrix<double, 3, 3> Matrix3;
-typedef ROOT::Math::SMatrix<double, 4, 4> Matrix4;
-typedef ROOT::Math::SMatrix<double, 3, 2> Matrix32;
-typedef ROOT::Math::SMatrix<double, 6, 6> Matrix6;
-typedef ROOT::Math::SMatrix<double, 2, 2, ROOT::Math::MatRepSym<double, 2>>
-    SymMatrix2;
-typedef ROOT::Math::SMatrix<double, 3, 3, ROOT::Math::MatRepSym<double, 3>>
-    SymMatrix3;
-typedef ROOT::Math::SMatrix<double, 4, 4, ROOT::Math::MatRepSym<double, 4>>
-    SymMatrix4;
-typedef ROOT::Math::SMatrix<double, 6, 6, ROOT::Math::MatRepSym<double, 6>>
-    SymMatrix6;
-typedef ROOT::Math::SVector<double, 2> Vector2;
-typedef ROOT::Math::SVector<double, 3> Vector3;
-typedef ROOT::Math::SVector<double, 4> Vector4;
-typedef ROOT::Math::SVector<double, 6> Vector6;
+// Templated vector types
+template <typename T, unsigned int kRows, unsigned int kCols>
+using Matrix = ROOT::Math::SMatrix<T, kRows, kCols>;
+template <typename T, unsigned int kSize>
+using SymMatrix =
+    ROOT::Math::SMatrix<T, kSize, kSize, ROOT::Math::MatRepSym<T, kSize>>;
+template <typename T, unsigned int kSize>
+using Vector = ROOT::Math::SVector<T, kSize>;
+
+// Commonly used vector and matrix types
+// For non-quadratic matrices the first number is the target dimensionality and
+// the second number is the source dimensionality.
+using Matrix2 = Matrix<double, 2, 2>;
+using Matrix23 = Matrix<double, 2, 3>;
+using Matrix26 = Matrix<double, 2, 6>;
+using Matrix3 = Matrix<double, 3, 3>;
+using Matrix32 = Matrix<double, 3, 2>;
+using Matrix4 = Matrix<double, 4, 4>;
+using Matrix6 = Matrix<double, 6, 6>;
+using SymMatrix2 = SymMatrix<double, 2>;
+using SymMatrix3 = SymMatrix<double, 3>;
+using SymMatrix4 = SymMatrix<double, 4>;
+using SymMatrix6 = SymMatrix<double, 6>;
+using Vector2 = Vector<double, 2>;
+using Vector3 = Vector<double, 3>;
+using Vector4 = Vector<double, 4>;
+using Vector6 = Vector<double, 6>;
 
 /** Squared Mahalanobis distance / norm of a vector.
  *
