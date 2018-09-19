c66e0c34dce6b21151a5737ed72c2d5892bc2831
Moritz Kiehn <msmk@cern.ch>
2018-09-19 20:44:51 +0200

    propagation: update to Eigen

--- lib/tracking/propagation.cpp
+++ lib/tracking/propagation.cpp
@@ -63,20 +63,19 @@ Storage::TrackState Tracking::propagate_to(const Storage::TrackState& state,
                                            const Mechanics::Plane& source,
                                            const Mechanics::Plane& target)
 {
+  // combined rotation from source to target system
+  Matrix3 combinedRotation =
+      target.rotationToLocal() * source.rotationToGlobal();
   // Transform to target local coordinates
   Vector3 uvw = target.toLocal(source.toGlobal(state.offset()));
-  Vector3 dir =
-      Transpose(target.rotation) * source.rotation * state.direction();
+  Vector3 dir = combinedRotation * state.direction();
   // Move to intersection w/ the target plane
   uvw -= dir * uvw[2] / dir[2];
 
-  // calculate offset jacobian.
-  // WARNING assumes vanishing slopes and needs to be amended.
-  Matrix2 jacOffset = Transpose(target.rotation.Sub<Matrix32>(0, 0)) *
-                      source.rotation.Sub<Matrix32>(0, 0);
-  // TODO 2017-10-10 msmk add full jacobian for all parameters
+  Matrix4 jac = jacobianState(state.direction(), combinedRotation);
+  SymMatrix4 cov = transformCovariance(jac, state.cov());
 
-  Storage::TrackState propagated(uvw.Sub<Vector2>(0), dir);
-  propagated.setCovOffset(Similarity(jacOffset, state.covOffset()));
+  Storage::TrackState propagated(uvw.head<2>(), dir);
+  propagated.setCov(cov);
   return propagated;
 }
