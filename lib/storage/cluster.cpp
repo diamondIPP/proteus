532e30f9a101ba6aa885001aff640de23f46f9a0
Moritz Kiehn <msmk@cern.ch>
2018-10-10 15:33:29 +0200

    storage/cluster: use Eigen

--- lib/storage/cluster.cpp
+++ lib/storage/cluster.cpp
@@ -12,12 +12,14 @@
 PT_SETUP_LOCAL_LOGGER(Cluster)
 
 Storage::Cluster::Cluster()
-    : m_cr(std::numeric_limits<double>::quiet_NaN(),
-           std::numeric_limits<double>::quiet_NaN())
-    , m_uv(std::numeric_limits<double>::quiet_NaN(),
-           std::numeric_limits<double>::quiet_NaN())
+    : m_col(std::numeric_limits<float>::quiet_NaN())
+    , m_row(std::numeric_limits<float>::quiet_NaN())
+    , m_u(std::numeric_limits<float>::quiet_NaN())
+    , m_v(std::numeric_limits<float>::quiet_NaN())
     , m_time(std::numeric_limits<float>::quiet_NaN())
     , m_value(std::numeric_limits<float>::quiet_NaN())
+    , m_crCov(SymMatrix2::Zero())
+    , m_uvCov(SymMatrix2::Zero())
     , m_index(kInvalidIndex)
     , m_track(kInvalidIndex)
     , m_matchedState(kInvalidIndex)
@@ -26,26 +28,16 @@ Storage::Cluster::Cluster()
 
 void Storage::Cluster::setPixel(const Vector2& cr, const SymMatrix2& cov)
 {
-  m_cr = cr;
-  m_crCov = cov;
-}
-
-void Storage::Cluster::setPixel(float col,
-                                float row,
-                                float stdCol,
-                                float stdRow)
-{
-  m_cr[0] = col;
-  m_cr[1] = row;
-  m_crCov(0, 0) = stdCol * stdCol;
-  m_crCov(1, 1) = stdRow * stdRow;
-  m_crCov(0, 1) = m_crCov(1, 0) = 0;
+  m_col = cr[0];
+  m_row = cr[1];
+  m_crCov = cov.selfadjointView<Eigen::Lower>();
 }
 
 void Storage::Cluster::setLocal(const Vector2& uv, const SymMatrix2& cov)
 {
-  m_uv = uv;
-  m_uvCov = cov;
+  m_u = uv[0];
+  m_v = uv[1];
+  m_uvCov = cov.selfadjointView<Eigen::Lower>();
 }
 
 bool Storage::Cluster::hasRegion() const
@@ -87,9 +79,9 @@ void Storage::Cluster::print(std::ostream& os, const std::string& prefix) const
   auto stdevCR = extractStdev(m_crCov);
   auto stdevUV = extractStdev(m_uvCov);
   os << prefix << "size: " << size() << '\n';
-  os << prefix << "pixel: [" << m_cr[0] << ", " << m_cr[1] << "]\n";
+  os << prefix << "pixel: [" << m_col << ", " << m_row << "]\n";
   os << prefix << "pixel stdev: [" << stdevCR[0] << ", " << stdevCR[1] << "]\n";
-  os << prefix << "local: [" << m_uv[0] << ", " << m_uv[1] << "]\n";
+  os << prefix << "local: [" << m_u << ", " << m_v << "]\n";
   os << prefix << "local stdev: [" << stdevUV[0] << ", " << stdevUV[1] << "]\n";
   os.flush();
 }
