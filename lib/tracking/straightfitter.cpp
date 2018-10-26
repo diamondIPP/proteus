fe84ce8049a7d1ea30584aa95cf2ca43e9ffc98e
Moritz Kiehn <msmk@cern.ch>
2018-10-26 14:05:41 +0200

    straightfitter: use common fitter implementation

--- lib/tracking/straightfitter.cpp
+++ lib/tracking/straightfitter.cpp
@@ -10,6 +10,42 @@
 #include "storage/event.h"
 #include "tracking/linefitter.h"
 
+// Fit the track in the local target plane
+static void fitLocal(const Storage::Track& track,
+                     const Mechanics::Geometry& geo,
+                     const Mechanics::Plane& target,
+                     Storage::TrackState& state,
+                     float& chi2,
+                     int& dof,
+                     Index ignoreSensorId = kInvalidIndex)
+{
+  Tracking::LineFitter3D fitter;
+
+  for (const auto& ci : track.clusters()) {
+    // only add the cluster if it is not ignored
+    if (ci.first == ignoreSensorId) {
+      continue;
+    }
+    const auto& source = geo.getPlane(ci.first);
+    const Storage::Cluster& cluster = ci.second;
+
+    // source local to target local assuming a position on the local source plane
+    Vector3 uvw = target.toLocal(source.toGlobal(cluster.posLocal()));
+    // the extra eval is needed to make older Eigen/compiler versions happy
+    Matrix2 jac = target.rotationToLocal().eval().topRows<2>() *
+                  source.rotationToGlobal().leftCols<2>();
+    SymMatrix2 cov = transformCovariance(jac, cluster.covLocal());
+
+    fitter.addPoint(uvw[0], uvw[1], uvw[2], 1 / cov(0, 0), 1 / cov(1, 1));
+  }
+
+  fitter.fit();
+  state = Storage::TrackState(fitter.params());
+  state.setCov(fitter.cov());
+  chi2 = fitter.chi2();
+  dof = fitter.dof();
+}
+
 Tracking::StraightFitter::StraightFitter(const Mechanics::Device& device)
     : m_geo(device.geometry()), m_sensorIds(device.sensorIds())
 {
@@ -19,21 +55,23 @@ std::string Tracking::StraightFitter::name() const { return "StraightFitter"; }
 
 void Tracking::StraightFitter::execute(Storage::Event& event) const
 {
+  const Mechanics::Plane kGlobal; // default plane is the global plane
+  Storage::TrackState state;
+  float chi2;
+  int dof;
+
   for (Index itrack = 0; itrack < event.numTracks(); ++itrack) {
     Storage::Track& track = event.getTrack(itrack);
 
     // global fit for common goodness-of-fit
-    fitStraightTrackGlobal(m_geo, track);
+    fitLocal(track, m_geo, kGlobal, state, chi2, dof);
+    track.setGlobalState(state);
+    track.setGoodnessOfFit(chi2, dof);
 
     // local fit for optimal parameters/covariance on each sensor plane
     for (Index iref : m_sensorIds) {
-      const Mechanics::Plane& ref = m_geo.getPlane(iref);
-
-      LineFitter3D fitter;
-      for (const auto& c : track.clusters())
-        fitter.addPoint(c.second, m_geo.getPlane(c.first), ref);
-      fitter.fit();
-      event.getSensorEvent(iref).setLocalState(itrack, fitter.state());
+      fitLocal(track, m_geo, m_geo.getPlane(iref), state, chi2, dof);
+      event.getSensorEvent(iref).setLocalState(itrack, state);
     }
   }
 }
@@ -51,24 +89,23 @@ std::string Tracking::UnbiasedStraightFitter::name() const
 
 void Tracking::UnbiasedStraightFitter::execute(Storage::Event& event) const
 {
+  const Mechanics::Plane kGlobal; // default plane is the global plane
+  Storage::TrackState state;
+  float chi2;
+  int dof;
+
   for (Index itrack = 0; itrack < event.numTracks(); ++itrack) {
     Storage::Track& track = event.getTrack(itrack);
 
     // global fit for common goodness-of-fit
-    fitStraightTrackGlobal(m_geo, track);
+    fitLocal(track, m_geo, kGlobal, state, chi2, dof);
+    track.setGlobalState(state);
+    track.setGoodnessOfFit(chi2, dof);
 
     // local fit for optimal parameters/covariance on each sensor plane
     for (Index iref : m_sensorIds) {
-      const Mechanics::Plane& ref = m_geo.getPlane(iref);
-
-      LineFitter3D fitter;
-      for (const auto& c : track.clusters()) {
-        if (c.first == iref)
-          continue;
-        fitter.addPoint(c.second, m_geo.getPlane(c.first), ref);
-      }
-      fitter.fit();
-      event.getSensorEvent(iref).setLocalState(itrack, fitter.state());
+      fitLocal(track, m_geo, m_geo.getPlane(iref), state, chi2, dof, iref);
+      event.getSensorEvent(iref).setLocalState(itrack, state);
     }
   }
 }
