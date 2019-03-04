dc3a65935333c9d202b22177d2a534b6b97d959b
Moritz Kiehn <msmk@cern.ch>
2019-03-04 18:57:16 +0100

    add track fitter setup functions

---
+++ lib/tracking/setupfitter.cpp
@@ -0,0 +1,45 @@
+/**
+ * \file
+ * \author Moritz Kiehn (msmk@cern.ch)
+ * \date 2019-03
+ */
+
+#include "setupfitter.h"
+
+#include <memory>
+
+#include "loop/eventloop.h"
+#include "straightfitter.h"
+#include "utils/logger.h"
+
+PT_SETUP_GLOBAL_LOGGER
+
+void Tracking::setupTrackFitter(const Mechanics::Device& device,
+                                const std::string& type,
+                                Loop::EventLoop& loop)
+{
+  if (type.empty()) {
+    INFO("no track fitter is configured");
+  } else if (type == "straight3d") {
+    loop.addProcessor(std::make_shared<Straight3dFitter>(device));
+  } else if (type == "straight4d") {
+    loop.addProcessor(std::make_shared<Straight4dFitter>(device));
+  } else {
+    FAIL("unknown configured track fitter '", type, "'");
+  }
+}
+
+void Tracking::setupUnbiasedTrackFitter(const Mechanics::Device& device,
+                                        const std::string& type,
+                                        Loop::EventLoop& loop)
+{
+  if (type.empty()) {
+    INFO("no track fitter is configured");
+  } else if (type == "straight3d") {
+    loop.addProcessor(std::make_shared<UnbiasedStraight3dFitter>(device));
+  } else if (type == "straight4d") {
+    loop.addProcessor(std::make_shared<UnbiasedStraight4dFitter>(device));
+  } else {
+    FAIL("unknown configured track fitter '", type, "'");
+  }
+}
