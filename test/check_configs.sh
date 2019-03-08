3ea8597687575e66d08bec05db5eb57a9b1cf27c
Moritz Kiehn <msmk@cern.ch>
2019-03-08 13:52:15 +0100

    test: generalize config check

--- test/check_configs.sh
+++ test/check_configs.sh
@@ -4,33 +4,27 @@
 
 set -ex
 
-echo "=== using $(which pt-cfg)"
+# syntax checks
 
-# pt-cfg should succeed for regular configs
-pt-cfg device.toml
-pt-cfg geometry/unigetel_ebeam012_nparticles01.toml
-pt-cfg geometry/unigetel_ebeam012_nparticles01-telescope.toml
-pt-cfg geometry/unigetel_ebeam012_nparticles01-telescope_first_last.toml
-pt-cfg geometry/unigetel_ebeam120_nparticles01.toml
-pt-cfg geometry/unigetel_ebeam120_nparticles01-telescope.toml
-pt-cfg geometry/unigetel_ebeam120_nparticles01-telescope_first_last.toml
-pt-cfg geometry/unigetel_ebeam120_nparticles01_xygamma.toml
-pt-cfg geometry/unigetel_ebeam120_nparticles01_xygamma-telescope.toml
-pt-cfg geometry/unigetel_ebeam120_nparticles01_xygamma-telescope_first_last.toml
-pt-cfg masks/telescope_empty.toml
-pt-cfg masks/dut_empty.toml
+# toml library is not available on the ci; not really necessary
+# for cfg in $(find . -iname '*.toml'); do
+#   python -c "import toml; toml.load('${cfg}')"
+# done
 
-# pt-cfg should fail
-# NOTE the shell seems to treat `! <cmd>` as a single command. To ensure the
-# that the shell exits when pt-cfg unexpectedly succeeds, an additional
-# failing command `false` is needed.
-! pt-cfg devices/test_failure-missing_settings.toml || false
-! pt-cfg devices/test_failure-non_exclusive_regions.toml || false
+# value checks
 
-! pt-cfg geometry/ || false # directory
-! pt-cfg geometry/does-not-exists || false
+# failure tests w/ invalid configs
+! pt-cfg test_failure/ || false # directory
+! pt-cfg test_failure/does-not-exists || false
+for cfg in $(find test_failure -iname '*.toml'); do
+  # NOTE the shell seems to treat `! <cmd>` as a single command. To
+  # ensure the that the shell exits when pt-cfg unexpectedly
+  # succeeds, an additional failing command `false` is needed.
+  ! pt-cfg ${cfg} || false
+done
 
-! pt-cfg geometry/test_failure-missing_id.toml || false
-! pt-cfg geometry/test_failure-non_orthogonal.toml || false
-pt-cfg geometry/test_success-with_angles.toml
-pt-cfg geometry/test_success-with_directions.toml
+# everything else should parse w/o issues
+# NOTE tool configuration (analysis.toml) is not supported by pt-cfg
+for cfg in $(find  -iname '*.toml' -not -iname 'analysis.toml' -not -path './test_failure/*' -not -path './output/*'); do
+  pt-cfg ${cfg}
+done
--- test/check_toml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# check toml syntax on all config files
-
-set -ex
-
-for path in $(find . -iname '*.toml'); do
-    python -c "import toml; toml.load('${path}')"
-done
